1340cdbde49df7e73fa184f38814cba509726335
WinnerWSW <144226875+WinnerWSW@users.noreply.github.com>
2024-10-13 16:22:03 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# 安装 Rust 编译器（用于需要 Rust 的 Python 库）
+# 安装 Rust 编译器
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
-# 更新 Rust 的环境变量
+# 将 Rust 添加到 PATH
 source $HOME/.cargo/env
+
+# 确保 pip 最新
+pip install --upgrade pip
